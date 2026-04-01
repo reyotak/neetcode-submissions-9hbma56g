d29953580ed306ba5b771b23e275e542bf213a1a
Renan Takara <60493369+reyotak@users.noreply.github.com>
2026-04-01 16:45:02 -0300

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        // first read a number with the str len which is between keys
+        // if not a number, str finshed
+        auto ss = std::stringstream();
+        for (auto &s : strs) {
+            ss << '{';
+            ss << s.size();
+            ss << '}';
+            ss << s;
+        }
+        std::cout << ss.str();
+        return ss.str();
+    }
+
+    vector<string> decode(string s) {
+        auto output = vector<string>();
+        for (int i = 0; i < s.size(); i++) {
+            // consume the len and the string
+            
+            // ignore the first {
+            i++;
+            
+            // get the number until finding }
+            std::stringstream lenStr;
+            while (s[i] != '}') {
+                lenStr << s[i];
+                i++;
+            }
+
+            // convert string to int
+            int len;
+            lenStr >> len;
+
+            // add the string to the list
+            auto str = s.substr(i + 1, len);
+            output.push_back(str);
+            i += len;
+        }
+        return output;
+    }
+
+    bool isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+};
