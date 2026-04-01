93aa99e3712b4e5340178a1f095be9eeb28ec2cd
Renan Takara <60493369+reyotak@users.noreply.github.com>
2026-04-01 16:50:02 -0300

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        // first read a number with the str len which is before a #
+        // if not a number, str finshed
+        auto ss = std::stringstream();
+        for (auto &s : strs) {
+            ss << s.size();
+            ss << '#';
+            ss << s;
+        }
+        return ss.str();
+    }
+
+    vector<string> decode(string s) {
+        auto output = vector<string>();
+        for (int i = 0; i < s.size(); i++) {
+            // consume the len and the string
+            
+            // get the number until finding #
+            std::stringstream lenStr;
+            while (s[i] != '#') {
+                lenStr << s[i];
+                i++;
+            }
+
+            // convert string to int
+            int len;
+            lenStr >> len;
+
+            // add the string to the list
+            auto str = s.substr(i + 1, len);
+            output.push_back(str);
+            i += len;
+        }
+        return output;
+    }
+
+    bool isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+};
