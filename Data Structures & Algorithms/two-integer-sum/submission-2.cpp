80644bda794182994f234d12fd4f8ae545df26be
Renan Takara <60493369+reyotak@users.noreply.github.com>
2026-09-25 10:29:24 -0300

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> mp_nums_index;
+        // for each n, add it on the map with it index
+        // check if the map contains the two sum part
+        std::vector<int> output;
+        for (int i = 0; i < nums.size(); i++) {
+            auto two_sum_ptr =  mp_nums_index.find(target - nums[i]);
+            if (two_sum_ptr != mp_nums_index.end()) {
+                // found the two sum part
+                output.push_back(two_sum_ptr->second);
+                output.push_back(i);
+                return output;
+            }
+            mp_nums_index[nums[i]] = i;
+        }
+        return output;
+    }
+};
