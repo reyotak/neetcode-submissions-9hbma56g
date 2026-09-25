b7a557c022d34a437b4d0ac33e96e5ae64e0cbbd
Renan Takara <60493369+reyotak@users.noreply.github.com>
2026-09-25 12:25:35 -0300

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0, r = numbers.size() - 1;
+        int sum = numbers[l] + numbers[r];
+        while (sum != target) {
+            if (sum > target) {
+                r--;
+            } else {
+                l++;
+            }
+            sum = numbers[l] + numbers[r];
+        }
+        return {l + 1, r + 1};
+    }
+};
